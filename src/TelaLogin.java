9df527898c062bad655800bc49068aa409073232
Nicolasortiiz <90704818+Nicolasortiiz@users.noreply.github.com>
2024-11-10 15:05:40 -0300

    Update TelaLogin.java

--- src/TelaLogin.java
+++ src/TelaLogin.java
@@ -1,36 +1,21 @@
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
-import java.util.HashMap;
+import java.util.Scanner;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import org.mindrot.jbcrypt.BCrypt;
+
 import javax.crypto.SecretKey;
 
 public class TelaLogin {
-    private static final int MAX_TENTATIVAS = 3;
-    private HashMap<String, Integer> tentativasFalhas = new HashMap<>();
-
-    public boolean funcionamentoTelaLogin(String email, String senha, SecretKey chave) {
-        // Verifica se o usuário está bloqueado por exceder o número máximo de tentativas
-        if (tentativasFalhas.getOrDefault(email, 0) >= MAX_TENTATIVAS) {
-            System.out.println("Conta bloqueada devido a tentativas falhas de login.");
-            return false;
-        }
+    public boolean funcionamentoTelaLogin (String email, String senha, SecretKey chave) {
 
         FuncoesBD funcoesBD = new FuncoesBD();
-        String senhaDesc = Criptografia.descriptografa(senha, chave);
-        
-        boolean loginSucesso = funcoesBD.retornaUsuario(email, hashSenha(senhaDesc));
+        String senhaDesc = Criptografia.descriptografa(senha,chave);
 
-        if (loginSucesso) {
-            // Login bem-sucedido: zera contagem de tentativas falhas para o usuário
-            tentativasFalhas.put(email, 0);
-        } else {
-            // Incrementa a contagem de tentativas falhas
-            int tentativas = tentativasFalhas.getOrDefault(email, 0) + 1;
-            tentativasFalhas.put(email, tentativas);
-            System.out.println("Tentativa de login falhou. Tentativa " + tentativas + " de " + MAX_TENTATIVAS);
-        }
+        return funcoesBD.retornaUsuario(email, hashSenha(senhaDesc));
 
-        return loginSucesso;
     }
 
     public static boolean validarEmail(String email) {
@@ -53,7 +38,7 @@ public class TelaLogin {
         return null;
     }
 
-    public String enviaEmail(String email) {
-        return new FuncoesMail().enviarEmail(email, "Código Login");
+    public String enviaEmail(String email){
+        return new FuncoesMail().enviarEmail(email,"Código Login");
     }
 }
